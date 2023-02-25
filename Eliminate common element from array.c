2e9ffa93b5634ec0e91879c8fa5465abe4555b93
Eshan276 <104843984+Eshan276@users.noreply.github.com>
2023-02-25 16:14:40 +0530

    Update Eliminate common element from array.c

--- Eliminate common element from array.c
+++ Eliminate common element from array.c
@@ -2,7 +2,7 @@
 int main(){
     int a[5]={1,4,1,4,4};
     int i, j,n=5,k,m=0,x; //n is array size can be taken via scanf
-    for(i=0;i<n;i++){
+    for(i=0;i<n;i++){    //TO FIND HOW MANY ELEMENTS ARE REPEATING?@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
       for(j=i+1;j<5;j++){
         if(a[i]==a[j] && j!=i){  //for(x=j;x<4;x++){a[x]=a[x+1];}
           m=m+1;
